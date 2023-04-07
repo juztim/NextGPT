ff998a9abbc732d8eeeaf034ba9ada0038b76fa3
Tim Wüstenhagen <personal@juztim.dev>
2023-04-07 06:25:09 +0200

    Finish chatting function and switching chats

---
+++ public/js/custom.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+	// SideBars
+
+	if ($('#right-menu-toggle').length > 0) {
+		$('#right-menu-toggle').click(function(){
+			if ($('#right-menu').hasClass('show')) {
+				$('#right-menu').removeClass('show');
+				$('#content-holder').removeClass('right-open');
+			} else {
+				$('#right-menu').addClass('show');
+				$('#content-holder').addClass('right-open');
+			}	
+		});
+	}
+
+	if ($('#left-menu-toggle').length > 0) {
+		$('#left-menu-toggle').click(function(){
+			if ($('#left-menu').hasClass('show')) {
+				$('#left-menu').removeClass('show');
+				$('#content-holder').removeClass('left-open');
+			} else {
+				$('#left-menu').addClass('show');
+				$('#content-holder').addClass('left-open');
+			}	
+		});
+	}
+
+	// Automatically Show the Side Menu if class .open-rightmenu is added to body
+	function responsiveMenus() {
+		if (screen.width < 991) {
+			if ($('#right-menu-toggle').length > 0) {
+				if($('#right-menu').hasClass('show')) {
+					$('#right-menu').removeClass('show');
+				}
+			}
+
+			if ($('#left-menu-toggle').length > 0) {
+				if($('#left-menu').hasClass('show')) {
+					$('#left-menu').removeClass('show');
+				}
+			}
+		} else {
+
+			if ($('#right-menu-toggle').length > 0) {
+				if(!$('#right-menu').hasClass('show')) {
+					$('#right-menu').addClass('show');
+				}
+			}
+
+			if ($('#left-menu-toggle').length > 0) {
+				if(!$('#left-menu').hasClass('show')) {
+					$('#left-menu').addClass('show');
+				}
+			}
+
+		}
+	}
+
+	responsiveMenus();
+
+	// $( window ).resize(function(){
+	// 	responsiveMenus();
+	// });
+
+	if(screen.width < 767) {
+		if ($('#right-menu-toggle').length > 0 && $('#left-menu-toggle').length > 0) {
+			$('#right-menu-toggle').click(function(){
+				if($('#right-menu').hasClass('show')) {
+					$('#left-menu').removeClass('show');
+				}
+			});
+
+			$('#left-menu-toggle').click(function(){
+				if($('#left-menu').hasClass('show')) {
+					$('#right-menu').removeClass('show');
+				}
+			});
+			
+		}
+	}
+
+	// Folders	 
+	$('.folder-trigger').click(function(){
+	 	$(this).toggleClass('open-folder');
+	});
+
+});
