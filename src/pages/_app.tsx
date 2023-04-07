14f167c0c8f1f379d9be9b595263ca28da25a097
Tim Wüstenhagen <personal@juztim.dev>
2023-04-07 17:12:16 +0200

    Add Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from "@vercel/analytics/react";
 
 import { api } from "~/utils/api";
 
@@ -19,6 +20,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
       <SessionProvider session={session}>
         <Toaster position="bottom-center" />
         <Component {...pageProps} />
+        <Analytics />
       </SessionProvider>
       <Script src="/js/jquery-3.6.3.min.js" strategy="beforeInteractive" />
       <Script src="/js/bootstrap.bundle.min.js" strategy="beforeInteractive" />
