3c5bce577e70548233c3401e4918093d2f6c3520
Tim Wüstenhagen <personal@juztim.dev>
2023-04-08 05:29:27 +0200

    Fix Propagation when deleting prompt

--- src/components/promptPreview.tsx
+++ src/components/promptPreview.tsx
@@ -1,4 +1,6 @@
 import type { Prompt } from "@prisma/client";
+import { toast } from "react-hot-toast";
+import { api } from "~/utils/api";
 
 const PromptPreview = ({
   prompt,
@@ -7,14 +9,41 @@ const PromptPreview = ({
   prompt: Prompt;
   onOpenPrompt: (prompt: Prompt) => void;
 }) => {
+  const ctx = api.useContext();
+
+  const { mutate: deletePrompt } = api.openAi.deletePrompt.useMutation({
+    onSuccess: () => {
+      toast.success("Prompt deleted");
+      void ctx.openAi.getAllPrompts.refetch();
+    },
+    onError: (error) => {
+      toast.error("Error deleting prompt");
+    },
+  });
+
   return (
-    <div className="mb-2" onClick={() => onOpenPrompt(prompt)}>
+    <div
+      className="mb-2"
+      onClick={(e) => {
+        e.stopPropagation();
+        onOpenPrompt(prompt);
+      }}
+    >
       <label
         className="btn btn-secondary d-flex justify-content-start align-items-center w-100"
         htmlFor="btn2-2"
       >
         <span className="icon icon-propt me-2" />
         {prompt.name}
+        <button
+          className="btn-nostyle px-2"
+          onClick={(e) => {
+            e.stopPropagation();
+            deletePrompt({ id: prompt.id });
+          }}
+        >
+          <span className="icon icon-delete" />
+        </button>
       </label>
     </div>
   );
