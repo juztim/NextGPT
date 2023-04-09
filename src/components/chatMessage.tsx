daffedae8ad0ef4b07c165c80b3b4e1344e04d49
Tim Wüstenhagen <personal@juztim.dev>
2023-04-09 04:31:33 +0200

    Show user pfp in chat

--- src/components/chatMessage.tsx
+++ src/components/chatMessage.tsx
@@ -1,14 +1,22 @@
-﻿import Image from "next/image";
+﻿import { useSession } from "next-auth/react";
+import Image from "next/image";
 import AvatarUser from "~/images/avatar-user.jpg";
 
 const ChatMessage = ({ message }: { message: string }) => {
+  const { data } = useSession();
   return (
     <div className="chat-item">
       <div className="row justify-content-center py-5">
         <div className="col-xl-8 col-sm-10 col-11">
           <div className="d-flex">
             <div className="image me-4">
-              <Image src={AvatarUser} alt="Avatar User" className="img-fluid" />
+              <Image
+                src={data?.user.image ?? AvatarUser}
+                alt="Avatar User"
+                className="img-fluid"
+                width={30}
+                height={30}
+              />
             </div>
 
             <div>{message}</div>
