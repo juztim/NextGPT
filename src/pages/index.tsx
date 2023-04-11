460a68ca9ca27c366ed6771d48dd3c7d550983ca
Tim Wüstenhagen <personal@juztim.dev>
2023-04-11 16:42:56 +0200

    Fix error when generating title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -163,6 +163,7 @@ const Home: NextPage = () => {
     },
     onSuccess: () => {
       toast.success("Chat cleared");
+      stopGenerating.current = false;
       void ctx.openAi.getChat.refetch({ id: activeChatId });
     },
   });
