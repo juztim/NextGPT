5041f919a93ed76180dd230c3819b7a981b63437
Tim Wüstenhagen <personal@juztim.dev>
2023-04-12 01:44:53 +0200

    Limit title creation

--- src/server/api/routers/openAiRouter.ts
+++ src/server/api/routers/openAiRouter.ts
@@ -565,7 +565,7 @@ export const OpenAiRouter = createTRPCRouter({
       const response = await openAI.createChatCompletion({
         messages: [
           {
-            content: `Please generate a title for the following sentence like ChatGPT does: ${input.message}`,
+            content: `Please generate a title for the following sentence like ChatGPT does: ${input.message}. Limit the title to 30 characters.`,
             role: "user",
           },
         ],
